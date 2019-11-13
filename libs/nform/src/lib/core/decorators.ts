ce5c2c197a89a19177d67f5955912813eba385aa
Shlomi Assaf <shlomiasaf@gmail.com>
2019-11-13 17:59:44 +0200

    WIP

--- libs/nform/src/lib/core/decorators.ts
+++ libs/nform/src/lib/core/decorators.ts
@@ -2,8 +2,8 @@ import { MetaClass } from '@pebula/metap/internal';
 
 import { FormModelMetadataArgs, FormModelMetadata, FormPropMetadata } from './metadata/index';
 
-// export const FormModel = MetaClass.decorator(FormModelMetadata, true);
 // We need to do some funky stuff for angular compiler...
 export const FormModel: (def?: FormModelMetadataArgs) => ClassDecorator = MetaClass['decorator'](FormModelMetadata, true);
+// export const FormModel = MetaClass.decorator(FormModelMetadata, true);
 
 export const FormProp = MetaClass.decorator(FormPropMetadata, true);
--- ssr/polyfills.ts
+++ ssr/polyfills.ts
@@ -1,3 +1,6 @@
+require('core-js/es6/reflect');
+require('core-js/es7/reflect');
+
 // working around:
 // https://github.com/angular/components/blob/461d5390d95732544db506fb6c6536f3a5803065/src/cdk-experimental/popover-edit/polyfill.ts#L40
 if (typeof global.Element === 'undefined') {
