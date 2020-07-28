e17ecc311a48f27b5beb5ba85ad9ec5c515a4b6d
Shlomi Assaf <shlomiasaf@gmail.com>
2020-07-28 15:49:55 +0300

    misc(metap): remove dependency in angular

--- libs/metap/jest.config.js
+++ libs/metap/jest.config.js
@@ -3,7 +3,5 @@ module.exports = {
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/metap',
   snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
   ]
 };
